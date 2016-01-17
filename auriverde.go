49909cb0b8f196a849426bddd6b085deb2fde988
Rob Lally <rob@robertlally.com>
2016-01-17 10:47:21 -0800

    .

--- auriverde.go
+++ auriverde.go
@@ -2,8 +2,43 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/cloudfoundry/cli/plugin"
 )
 
+type BasicPlugin struct{}
+
+func (c *BasicPlugin) Run(cliConnection plugin.CliConnection, args []string) {
+	if args[0] == "basic-plugin-command" {
+		fmt.Println("Running the basic-plugin-command")
+	}
+}
+
+func (c *BasicPlugin) GetMetadata() plugin.PluginMetadata {
+	return plugin.PluginMetadata{
+		Name: "MyBasicPlugin",
+		Version: plugin.VersionType{
+			Major: 1,
+			Minor: 0,
+			Build: 0,
+		},
+		MinCliVersion: plugin.VersionType{
+			Major: 6,
+			Minor: 7,
+			Build: 0,
+		},
+		Commands: []plugin.Command{
+			plugin.Command{
+				Name:     "basic-plugin-command",
+				HelpText: "Basic plugin command's help text",
+				UsageDetails: plugin.Usage{
+					Usage: "basic-plugin-command\n   cf basic-plugin-command",
+				},
+			},
+		},
+	}
+}
+
 func main() {
-    fmt.Printf("Hello world")    
+	plugin.Start(new(BasicPlugin))
 }
