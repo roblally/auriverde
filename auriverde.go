47f17f98075469f82ed2f97e9f515b1abd401ef8
Rob Lally <Robert_Lally@gap.com>
2016-01-19 09:29:57 -0800

    .

--- auriverde.go
+++ auriverde.go
@@ -9,30 +9,32 @@ import (
 type BasicPlugin struct{}
 
 func (c *BasicPlugin) Run(cliConnection plugin.CliConnection, args []string) {
-	if args[0] == "auriverde" {
+//	if args[0] == "auriverde" {
 		fmt.Println("Running the auriverde command")
-	}
+//	}
 }
+
+const helpText = "auriverde is a tool for blue-green deployment"
 
 func (c *BasicPlugin) GetMetadata() plugin.PluginMetadata {
 	return plugin.PluginMetadata{
-		Name: "Auriverde",
+		Name: "auriverde",
 		Version: plugin.VersionType{
-			Major: 1,
-			Minor: 0,
+			Major: 0,
+			Minor: 1,
 			Build: 0,
 		},
 		MinCliVersion: plugin.VersionType{
 			Major: 6,
-			Minor: 7,
+			Minor: 0,
 			Build: 0,
 		},
 		Commands: []plugin.Command{
 			plugin.Command{
 				Name:     "auriverde",
-				HelpText: "Some help...",
+				HelpText: helpText,
 				UsageDetails: plugin.Usage{
-					Usage: "auriverde\n   cf auriverde",
+					Usage: "cf auriverde",
 				},
 			},
 		},
